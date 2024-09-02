396f5ea15e32bb809b4f9331b253bd964c096c93
Diana <dianadorojuc3@gmail.com>
2024-09-02 15:58:49 +0200

    update favourites

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -1,15 +1,30 @@
-import { Container, ListGroup, ListGroupItem } from "react-bootstrap"
-import Job from "./Job"
+import { Button, Container, ListGroup, ListGroupItem } from "react-bootstrap";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 
-const Favourites = () =>{
+const Favourites = () => {
+  const myList = useSelector((state) => state.myList);
+  const dispatch = useDispatch();
+  return (
     <Container>
-       
-            <ListGroup>
-                <ListGroupItem>
-             
-               
-                </ListGroupItem>
-            </ListGroup>
+      <h2>My list of favourite companies</h2>
+      <ListGroup>
+        {myList.map((company, i) => (
+          <ListGroupItem key={i}>
+            <Link to={`${company}`}>{company}</Link>
+            <Button
+              variant="danger"
+              className="ml-3"
+              onClick={() =>
+                dispatch({ type: "REMOVE_FROM_FAVOURITES", payload: company })
+              }
+            >
+              Remove
+            </Button>
+          </ListGroupItem>
+        ))}
+      </ListGroup>
     </Container>
-}
-export default  Favourites
+  );
+};
+export default Favourites;
