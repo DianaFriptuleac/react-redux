75b6c622a2844448e6db78451255c774e81c6c1b
Diana <dianadorojuc3@gmail.com>
2024-09-02 15:50:09 +0200

    update redux and Company

--- src/components/CompanySearchResults.jsx
+++ src/components/CompanySearchResults.jsx
@@ -1,18 +1,20 @@
 import { useEffect, useState } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import Job from "./Job";
 import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 const CompanySearchResults = () => {
   const [jobs, setJobs] = useState([]);
   const params = useParams();
+  const dispatch = useDispatch()
+  const myList = useSelector(state => state.myList)
 
   const baseEndpoint = "https://strive-benchmark.herokuapp.com/api/jobs?company=";
 
   useEffect(() => {
     getJobs();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [params.company]);
 
   const getJobs = async () => {
     try {
@@ -28,11 +30,22 @@ const CompanySearchResults = () => {
     }
   };
 
+  const handleList = ()=>{
+if(myList.includes(params.company)) {
+  dispatch({type: "REMOVE_FROM_FAVOURITES",payload:params.company})
+}else{
+  dispatch ({type:"ADD_TO_FAVOURITES", payload:params.company})
+}
+  }
+
   return (
     <Container>
       <Row>
         <Col className="my-3">
           <h1 className="display-4">Job posting for: {params.company}</h1>
+          <Button onClick={handleList}>
+            {myList.includes(params.company) ?"Remouve" : "Add"}
+          </Button>
           {jobs.map(jobData => (
             <Job key={jobData._id} data={jobData} />
           ))}
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,40 +2,39 @@
 
 //redux ha bisogn di un valore  iniziale per lo stato
 const initialState = {
-    //rganizzo in sotto-oggetti
-    listOfJobs: {
-        content: [],
+  //organizzo in sotto-oggetti
+  listOfJobs: {
+    content: [],
+  },
+  //scrivo un nuovo stato per le aziende preferite
+  myList: [],
+};
+
+//initialState- il valore di defaul di state
+const mainReducer = (state = initialState, action) => {
+  //state- stato corrente dii redux
+  //action-azione che e stata appena dispachata
+
+  switch (action.type) {
+    case "ADD_TO_FAVOURITES":
+      return {
+        //ritorno il nuovo stato di redux
+        //ritorno un nuovo oggetto
+        ...state,
+       myList: [...state.myList, action.payload],
+        },
+
+        case 'REMOVE_FROM_FAVOURITES':
+            return {
+                ...state,
+                favouriteCompanies: state.favouriteCompanies.filter(
+                    (company) => company !== action.payload
+                )
+            }
+
+        default:
+            return state 
     }
 }
 
-//initialState- il valore di defaul di state
-const mainReducer = (state = initialState, action) =>{
-//state- stato corrente dii redux
-//action-azione che e stata appena dispachata
-
-   switch(action.type)  {
-
-    case 'ADD_TO_FAVOURITES':
-        return{
-            //ritorno il nuovo stato di redux
-            //ritorno un nuovo oggetto
-            ...state,
-            listOfJobs: {
-                ...state.listOfJobs,
-                content: [...state.listOfJobs.content, action.payload],
-            }
-        }
-
-        case 'REMOVE_FROM_FAVOURITES':
-            return{
-                ...state,
-                listOfJobs: {
-                    ...state.listOfJobs,
-                    content: state.listOfJobs.content.filter((jobData, _id) => _id !== action.payload)
-                }
-            }
-    default:
-        return state 
-   }
-}
-export default mainReducer
+export default mainReducer;
